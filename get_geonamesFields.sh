adecb5e58effb4581d26b7fb9e4154341582229c
Albert Decatur <adecatur@aiddata.org>
2014-06-16 11:34:13 -0400

    also get feature code along with feature class name when pulling from geonames api

--- get_geonamesFields.sh
+++ get_geonamesFields.sh
@@ -5,7 +5,7 @@
 
 
 # print header
-echo -e "geonameID\tplacename\tlatitude\tlongitude\tlocation_type\tgeonames_ADMcode\tgeonames_ADMname\tgeonamesAPI_retrievalTime"
+echo -e "geonameID\tplacename\tlatitude\tlongitude\tlocation_type_code\tlocation_type\tgeonames_ADMcode\tgeonames_ADMname\tgeonamesAPI_retrievalTime"
 
 cat $1|\
 # remove header
@@ -18,24 +18,46 @@ uniq|\
 # for every geonameID return desired elements from geonames API
 parallel --gnu '
 	# get all the elements from the API we want later
-	elements=$( 
-		curl -s "http://api.geonames.org/get?geonameId={}&username='$3'" |\
-		grep -E "<countryCode>[^<]*</countryCode>|<countryName>[^<]*</countryName>|<name>[^<]*</name>|<lat>[^<]*</lat>|<lng>[^<]*</lng>|<fcodeName>[^<]*</fcodeName>|<admin" 
-	)
-	# get the current time in seconds UTC
-	time=$( date -u --iso-8601=seconds )
-	# get pipe separated list of both ADM codes and ADM names
-	country_code=$( echo "$elements" | grep -E "<countryCode>[^<]*</countryCode>" | grep -oE ">[^<]*<" | sed "s:>\|<::g")
-	country_name=$( echo "$elements" | grep -E "<countryName>[^<]*</countryName>" | grep -oE ">[^<]*<" | sed "s:>\|<::g")
-	admFields=$( echo "$elements" | grep -E "<admin" )
-	admCodes=$( echo "$admFields" | grep "^<adminCode" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
-	admNames=$( echo "$admFields" | grep "^<adminName" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
-	admCode=$( echo "$admCodes" | sed "s:|::g" | tr "\n" "|" | sed "s:|$::g" | sed "s:^:$country_code|:g" )
-	admName=$( echo "$admNames" | sed "s:|::g" | tr "\n" "|" | sed "s:|$::g" | sed "s:^:$country_name|:g" )
-	# get other elements
-	lat=$( echo "$elements" | grep -E "<lat>[^<]*</lat>" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
-	lng=$( echo "$elements" | grep -E "<lng>[^<]*</lng>" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
-	name=$( echo "$elements" | grep -E "<name>[^<]*</name>" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
-	fcodeName=$( echo "$elements" | grep -E "<fcodeName>[^<]*</fcodeName>" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
-	echo -e "{}\t$name\t$lat\t$lng\t$fcodeName\t$admCode\t$admName\t$time"
+	get_elements() {
+		elements=$( 
+			curl -s "http://api.geonames.org/get?geonameId={}&username='$3'" |\
+			grep -E "<countryCode>[^<]*</countryCode>|<countryName>[^<]*</countryName>|<name>[^<]*</name>|<lat>[^<]*</lat>|<lng>[^<]*</lng>|<fcode>[^<]*</fcode>|<fcodeName>[^<]*</fcodeName>|<admin" 
+		)
+	}
+	# check if the API returned null
+	# can happen even when geonameid truly is retrievable
+	check_null() {
+		if [[ -z "$elements" ]]; then
+			echo 1
+		fi
+	}
+	# print elements we want from API as a TSV
+	elements_as_tsv() {
+		# get the current time in seconds UTC
+		time=$( date -u --iso-8601=seconds )
+		# get pipe separated list of both ADM codes and ADM names
+		country_code=$( echo "$elements" | grep -E "<countryCode>[^<]*</countryCode>" | grep -oE ">[^<]*<" | sed "s:>\|<::g")
+		country_name=$( echo "$elements" | grep -E "<countryName>[^<]*</countryName>" | grep -oE ">[^<]*<" | sed "s:>\|<::g")
+		admFields=$( echo "$elements" | grep -E "<admin" )
+		admCodes=$( echo "$admFields" | grep "^<adminCode" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
+		admNames=$( echo "$admFields" | grep "^<adminName" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
+		admCode=$( echo "$admCodes" | sed "s:|::g" | tr "\n" "|" | sed "s:|$::g" | sed "s:^:$country_code|:g" )
+		admName=$( echo "$admNames" | sed "s:|::g" | tr "\n" "|" | sed "s:|$::g" | sed "s:^:$country_name|:g" )
+		# get other elements
+		lat=$( echo "$elements" | grep -E "<lat>[^<]*</lat>" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
+		lng=$( echo "$elements" | grep -E "<lng>[^<]*</lng>" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
+		name=$( echo "$elements" | grep -E "<name>[^<]*</name>" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
+		fcode=$( echo "$elements" | grep -E "<fcode>[^<]*</fcode>" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
+		fcodeName=$( echo "$elements" | grep -E "<fcodeName>[^<]*</fcodeName>" | grep -oE ">[^<]*<" | sed "s:>\|<::g" )
+		echo -e "{}\t$name\t$lat\t$lng\t$fcode\t$fcodeName\t$admCode\t$admName\t$time"
+	}
+	get_elements
+	# if API returned null then try again, waiting for 2s this time
+	if [[ -n $( check_null ) ]]; then
+		get_elements
+		sleep 2
+		elements_as_tsv
+	else
+		elements_as_tsv
+	fi
 '
